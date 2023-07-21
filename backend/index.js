478e8ecc4d26734b27c4b8a1e61a4ec73a33119e
sauravs4h <sauravs4h@gmail.com>
2023-07-21 20:07:52 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send({msg:"this is base api",status:"success"});
+    
+})
+
+app.listen(8080,()=>{
+    try {
+        console.log("running on 8080")
+        
+    } catch (error) {
+        console.log("error while running")
+    }
+})
